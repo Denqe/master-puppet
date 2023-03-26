2919e141e2191eedcb3821a8012173a3d45b9ae6
Denis Zdor <126823973+Denqe@users.noreply.github.com>
2023-03-26 19:47:36 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,11 +2,8 @@ node master.puppet{
   package { 'git':
     ensure => installed,
   }
-  package { 'r10k':
-    ensure => installed,
-  }
   service { 'firewalld':
-    ensure => stoped,
+    ensure => stopped,
   }
   
 
