dda195079f0b7a5bcd43a4ccdc6e2b04a3bd840f
Denis Zdor <126823973+Denqe@users.noreply.github.com>
2023-03-27 16:38:08 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,41 @@
-node master.puppet{
-  package { 'htop':
-    ensure => installed,
-  }
+node slave1.puppet{
+class html {
+  package {['httpd']:
+    ensure => installed
+ }
   
+  file { '/var/www/html':
+    ensure => directory
+ }
+  
+  file { '/var/www/html/index.html':
+    ensure => file,
+    source => 'puppet:///modules/html/index.html'
+ }
+   
+  service {'httpd':
+    ensure => running
+ }
+ }
+ }
 
-
-
+node slave2.puppet{
+class php {
+  package {['httpd', 'php']:
+    ensure => installed
+ }
+  
+  file { '/var/www/html':
+    ensure => directory
+ }
+  
+  file { '/var/www/html/index.php':
+    ensure => file,
+    source => 'puppet:///modules/php/index.php'
+ }
+  
+  service {'httpd':
+    ensure => running
+}
+}  
 }
