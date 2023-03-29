aec2d220aa434aee156cd893b7b578869233f36f
Denis Zdor <126823973+Denqe@users.noreply.github.com>
2023-03-29 22:48:02 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,40 +1,55 @@
 node 'slave1.puppet'{
   package {'httpd':
     ensure => installed,
- }
+}
   
   service {'firewalld':
     ensure => stopped,
- }
+}
   
   file { '/var/www/html/index.html':
     ensure => file,
     source => '/vagrant/index.html'
- }
+}
    
   service {'httpd':
     ensure => running,
- }
- }
+}
+}
 
 node 'slave2.puppet'{
   package {'httpd':
     ensure => installed,
- }
+}
   
   package {'php':
     ensure => installed,
- }
+}
   
   service {'firewalld':
     ensure => stopped,
- } 
+} 
   file {'/var/www/html/index.php':
     ensure => file,
     source => '/vagrant/index.php'
- }
+}
   
   service {'httpd':
     ensure => running,
 }  
+}
+
+node 'master.puppet'{
+  package {'nginx':
+    ensure => installed,
+}
+
+  file {'/etc/nginx/sites-available/default':
+    ensure => file,
+    source => '/vagrant/default'
+}
+
+  service {'nginx':
+    ensure => restarted,
+} 
 }
