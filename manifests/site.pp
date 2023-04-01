b4cb2ebec8d33638620168d138ebb72f2f0d9c3a
Denis Zdor <126823973+Denqe@users.noreply.github.com>
2023-04-01 12:03:52 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -57,3 +57,22 @@ node 'master.puppet'{
     ensure => running,
 } 
 }
+
+node 'master.puppet'{
+  package {'nginx':
+    ensure => installed,
+}
+
+  file {'/etc/nginx/sites-available':
+    ensure => directory
+}
+
+  file {'/etc/nginx/sites-available/default':
+    ensure => file,
+    source => '/vagrant/default'
+}
+
+  service {'nginx':
+    ensure => running,
+} 
+}
