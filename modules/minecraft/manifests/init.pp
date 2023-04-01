352eedacca26969a07dcf420ef9e21f9ecdddb68
Denis Zdor <126823973+Denqe@users.noreply.github.com>
2023-04-01 22:57:42 +0300

    Update init.pp

--- modules/minecraft/manifests/init.pp
+++ modules/minecraft/manifests/init.pp
@@ -9,12 +9,15 @@ class minecraft{
 
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
+    source => 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar',
+}
+
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
     source => 'puppet:///modules/minecraft/files/minecraft.service',
 }
 
   service {'minecraft.service':
         ensure => running,
 }
-
-
 }
