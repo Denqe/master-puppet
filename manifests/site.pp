a39b1f4e206aed622cfaf3c4f864be8c21b32c6b
Denis Zdor <126823973+Denqe@users.noreply.github.com>
2023-04-01 22:36:21 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -41,23 +41,19 @@ node 'slave2.puppet'{
 }
 
 node 'master.puppet'{
-class { 'nginx':
-  mail => true,
+
+include nginx
+  
+  nginx::resource::server {'static':
+    listen_port => 81,
+    proxy       => 'http://192.168.50.11:80',
 }
 
-#  nginx::resource::server {'192.168.50.10':
-#  www_root => '/opt/html/',
-#}
-  
- # nginx::resource::server {'static':
- #   listen_port => 81,
- #   proxy       => 'http://192.168.50.11:80',
-#}
+  nginx::resource::server {'dynamic':
+    listen_port => 8080,
+    proxy       => 'http://192.168.50.12:80',
+}
 
- # nginx::resource::server {'dynamic':
-  #  listen_port => 8080,
-   # proxy       => 'http://192.168.50.12:80',
-#}
   exec {'selinux':
     command     => 'setenforce 0',
     path        => ['/usr/bin', '/bin', '/usr/sbin'],
