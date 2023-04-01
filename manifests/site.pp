8c6fb5a7d69bdcbfd5510f3b381d60d5e4a9b259
Denis Zdor <126823973+Denqe@users.noreply.github.com>
2023-04-01 22:00:12 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -40,8 +40,12 @@ node 'slave2.puppet'{
 }
 
 node 'master.puppet'{
-  class{"nginx":
+  class{"nginx": }
+  
+  nginx::resource::server {'192.168.50.10':
+  www_root => '/opt/html/',
 }
+  
   nginx::resource::server {'static':
     listen_port => 81,
     proxy       => 'http://192.168.50.11:80',
