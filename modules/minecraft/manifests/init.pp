b0ec6fd2556b1eba24e12e9bdc409c203dcfd620
Denis Zdor <126823973+Denqe@users.noreply.github.com>
2023-04-02 14:22:17 +0300

    Update init.pp

--- modules/minecraft/manifests/init.pp
+++ modules/minecraft/manifests/init.pp
@@ -13,14 +13,14 @@ class minecraft{
     source => 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar',
 }
 
-  file {'/etc/systemd/system/minecraft.service':
-    ensure => file,
-    source => 'puppet:///modules/minecraft/files/minecraft.service',
-}
-
   file {'/opt/minecraft/eula.txt':
     ensure => file,
     source => 'puppet:///modules/minecraft/files/eula.txt',
+}
+
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/files/minecraft.service',
 }
 
   service {'minecraft.service':
